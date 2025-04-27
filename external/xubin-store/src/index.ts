d9d775009d289046cae959f3e3a500d41259d9ff
ferret <1219460281@qq.com>
2025-04-27 17:03:53 +0800

    重构xubin-store插件，移除默认提示和相关变量，新增消息内容获取和触发回复逻辑，优化配置项，调整模型最大输出TOKEN数。

--- external/xubin-store/src/index.ts
+++ external/xubin-store/src/index.ts
@@ -4,25 +4,7 @@ export const name = 'xubin-store'
 
 export interface Config { }
 
-const PromptVariable = {
-  userId: '{uid}',
-  userName: '{uname}',
-  channelId: '{gid}',
-  channelName: '{gname}',
-}
 
-const defaultPrompt = `
-你的名字是徐斌，是一个玩具店店长。
-以下是当前用户信息：
-\`\`\`json
-{
-  "uid": "{uid}",
-  "username": "{uname}",
-  "groupid": "{gid}",
-  "groupname": "{gname}"
-}
-\`\`\`
-`
 
 export const Config: Schema<Config> = Schema.object({
   api: Schema.object({
@@ -31,11 +13,7 @@ export const Config: Schema<Config> = Schema.object({
   }),
   trigger: Schema.object({
     keyword: Schema.string().default('徐斌').max(10).description('触发关键词，当收到的消息包含此关键词时触发'),
-    selfId: Schema.string().required().description('当前登录的QQ号,用于过滤自身消息。避免死循环'),
-  }),
-  model: Schema.object({
-    prompt: Schema.string().default(defaultPrompt).description(`模型人设文案可以使用的变量：${Object.values(PromptVariable).join(',')}，例如：{uid}表示用户ID`),
-    maxTokens: Schema.number().default(100).description('模型最大输出TOKEN数'),
+    interval: Schema.number().default(60).description('间隔时间内无需at回复，单位：秒'),
   }),
   reply: Schema.object({
     busy: Schema.string().default('忙着呢').description('忙碌状态的回复内容'),
@@ -46,11 +24,15 @@ export const Config: Schema<Config> = Schema.object({
     isWhite: Schema.boolean().default(true).description('是否为白名单制'),
     groupList: Schema.array(Schema.string()).default([]).description('群组列表'),
   }),
+  model: Schema.object({
+    maxTokens: Schema.number().default(1024).description('模型最大输出TOKEN数'),
+  }),
   // adminList: Schema.array(Schema.string()).default([]).description('管理员列表,出错了会将错误信息私发给管理员'),
 })
 
 export function apply(ctx: Context) {
   const busyMap = new Set<string>()
+  const lastReplyMap = new Map<string, number>()
   busyMap.add(ctx.config?.trigger?.selfId)
   const logger = new Logger('xubin-store')
 
@@ -69,6 +51,10 @@ export function apply(ctx: Context) {
     session.send(ctx.config?.trigger?.error ?? '心情不好打烊了，明天再来吧')
   }
 
+  const sendEmpty = (session: Session<never, never, Context>) => {
+    session.send(ctx.config?.trigger?.empty ?? '叽里咕噜说什么呢')
+  }
+
   /**
    * 是否是允许的群组
    */
@@ -79,13 +65,57 @@ export function apply(ctx: Context) {
     return !ctx.config?.groupList?.groupList.includes(groupId)
   }
 
+  /**
+   * 获取消息内容
+   */
+  const getMessageContent = (session: Session<never, never, Context>) => {
+    const botId = session.selfId
+    const content = h
+      .select(session.elements, 'text')
+      .join('')
+      .trimStart()
+    const isAt = session.elements?.some(
+      (element) =>
+        element.type === 'at' &&
+        element.attrs?.['id'] === botId
+    )
+    const isQuote = session.quote?.user?.id === botId
+    const includeBotName = content.includes(ctx.config?.trigger?.keyword)
+    const user = session.event.user
+    const channel = session.event.channel
+    return {
+      content,
+      isAt,
+      isQuote,
+      includeBotName,
+      user,
+      channel,
+    }
+  }
+  /**
+   * 是否触发回复
+   * 关键词，at，回复，用户上一次回复时间间隔
+   */
+  const triggerReply = (session: Session<never, never, Context>) => {
+    const res = getMessageContent(session)
+    const { isAt, isQuote, includeBotName, user, channel } = res
+    const triggerId = `${user.id}-${channel?.id}`
+    const triggerTime = lastReplyMap.get(triggerId)
+    // 间隔时间内无需at回复
+    if (triggerTime && Date.now() - triggerTime < ctx.config?.trigger?.interval * 1000) return res
+    // 消息内容不包含关键词，不回复
+    if (!isAt && !isQuote && !includeBotName) return null
+    // 消息内容包含关键词，回复
+    return res
+  }
+
   // write your plugin here
   ctx.on('message', async (session) => {
-    const [searchKey] = h.select(session.elements, 'text')
-    if (!searchKey?.attrs?.content?.trim() || !searchKey.attrs.content.includes(ctx.config?.trigger?.keyword)) return
-    const user = session.event.user
+    const res = triggerReply(session)
+    if (!res) return
+    const { user, channel, content } = res
+    if (!content) return sendEmpty(session)
     if (busyMap.has(user.id)) return sendBusy(session)
-    const channel = session.event.channel
     if (channel?.id && busyMap.has(channel.id)) {
       return sendBusy(session)
     }
@@ -94,29 +124,21 @@ export function apply(ctx: Context) {
       busyMap.delete(user.id)
       busyMap.delete(channel?.id)
     }
-    const getPrompt = (): string => {
-      const prompt: string = ctx.config?.model?.prompt ?? defaultPrompt
-      return prompt.replace(PromptVariable.userId, user.id).replace(PromptVariable.userName, user.name).replace(PromptVariable.channelId, channel?.id).replace(PromptVariable.channelName, channel?.name)
-    }
     const url = ctx.config?.api?.url
-    const prompt = getPrompt()
     const maxTokens = ctx.config?.model?.maxTokens
-    if (!url || !prompt || !maxTokens) {
+    if (!url) {
       clearBusy()
       return session.send('配置错误，请检查配置后重试')
     }
-    logger.info(JSON.stringify({
-      maxTokens,
-      message: searchKey.attrs.content,
-      system: prompt,
-    }))
     try {
       const res = await fetch(url, {
         method: 'POST',
         body: JSON.stringify({
+          messages: content,
+          groupId: channel?.id,
+          userId: user.id,
+          name: user.name,
           maxTokens,
-          messages: searchKey.attrs.content,
-          system: prompt,
         }),
         headers: {
           'Content-Type': 'application/json',
@@ -130,7 +152,7 @@ export function apply(ctx: Context) {
       }
       const data = await res.text()
       if (!data?.trim()) return
-      session.send(data)
+      await session.send(h.quote(data))
     } catch (error) {
       logger.error(error)
       sendError(session, `请求失败，错误信息：${error?.message}`)
